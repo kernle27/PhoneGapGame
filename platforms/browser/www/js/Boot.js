cf463f13fa1eee07eab3867dee1bdbcdf7e17eac
Dante\Dantezoz <kernle27@gmail.com>
2017-08-05 12:15:45 +0200

    First Commit

---
+++ platforms/browser/www/js/Boot.js
@@ -0,0 +1,9 @@
+///<reference path="../js/phaser.js" />
+
+var game = null;
+
+function onDeviceReady () {
+    
+}
+
+document.addEventListener("deviceready", onDeviceReady, false );
---
+++ www/js/Boot.js
@@ -0,0 +1,37 @@
+
+var game  = null;
+
+function onDeviceReady () {
+    game = new Phaser.Game(window.innerWidth, window.innerHeight, Phaser.AUTO);
+        
+    game.state.add( "Boot", Boot );
+
+    game.state.start( "Boot" );
+}
+
+document.addEventListener("deviceready", onDeviceReady, false );
+
+
+class Boot extends Phaser.State {
+
+    constructor() {
+        
+
+        this.init = () => {
+            
+        }
+
+
+        this.preload = () => {
+
+        }
+
+
+        this.create = () => {
+
+        }
+
+        
+    }
+
+}
